964a89c9d4097d7f3db7e82909aea201305432fc
isliangj <isliangj@icloud.com>
2017-09-22 21:33:27 +0800

    Vue-music
    Vue音乐播放器demo

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+
+Vue.config.productionTip = false
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  template: '<App/>',
+  components: { App }
+  // render: h => h(App)
+})
