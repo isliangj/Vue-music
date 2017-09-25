3e2ef86b9e3a8c4c98104f139a7565e6cec7d9fb
isliangj <isliangj@icloud.com>
2017-09-25 20:54:38 +0800

    feature(header): header结构样式及路由的配置

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
 
 import 'common/stylus/index.styl'
 
+// 解决点击300毫秒延迟问题
+fastclick.attach(document.body)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   template: '<App/>',
   components: { App }
   // render: h => h(App)
