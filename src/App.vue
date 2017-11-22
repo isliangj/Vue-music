d868da9b59584f320d0ce0642565eb1267732fbf
liangj <isliangj@icloud.com>
2017-11-22 21:23:40 +0800

    init(program): 用 Vue-cli 搭建项目, 项目初始化完成

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+
+#app {
+  color: #111;
+}
+</style>
